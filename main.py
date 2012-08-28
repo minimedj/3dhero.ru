879faa509ccba928d4288ec6e249cc844b1e44a1
Serg Baburin <serg.baburin@gmail.com>
2012-08-28 11:25:26 +0400

    Sync with my-gae-init (fixed import lib.zip).

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
+if not ('lib.zip' in sys.path):
+    sys.path.insert(0, 'lib.zip')
 import flask
 import config
 from mgi import install_apps, install_login_manager
-
-if not ('lib.zip' in sys.path):
-    sys.path.insert(0, 'lib.zip')
 
 app = flask.Flask(__name__)
 app.config.from_object(config)
