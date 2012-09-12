b0da08295ad0e00eb87725c7f302279c73b1b5a6
Serg Baburin <serg.baburin@gmail.com>
2012-09-12 11:39:07 +0400

    Added out products to main page. Added paginator (from kay-framework).

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import flask
+from apps.product.models import Product
+from apps.utils.paginator import Paginator, EmptyPage, InvalidPage
 
 mod = flask.Blueprint(
     "pages",
@@ -7,12 +9,22 @@ mod = flask.Blueprint(
     template_folder='templates'
 )
 
-@mod.route('/')
-def index():
+@mod.route('/', defaults={'page':1})
+@mod.route('/page/<int:page>/')
+def index(page):
+    products = Product.query(
+        Product.is_public == True,
+        Product.images_list.is_image == True).order(-Product.rating)
+    paginator = Paginator(products, 12)
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages())
     return flask.render_template(
         'pages/index.html',
         html_class='welcome',
         channel_name='welcome',
+        products=products
     )
 
 
