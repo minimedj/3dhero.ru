54dce51633dab0a20628c6ae6073255ab85acb69
Serg Baburin <serg.baburin@gmail.com>
2012-09-14 13:41:29 +0400

    Increased count of products in each page.

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -3,11 +3,14 @@ import flask
 from apps.product.models import Product
 from apps.utils.paginator import Paginator, EmptyPage, InvalidPage
 
+
 mod = flask.Blueprint(
     "pages",
     __name__,
     template_folder='templates'
 )
+
+PRODUCT_PER_PAGE = 16
 
 @mod.route('/', defaults={'page':1})
 @mod.route('/page/<int:page>/')
@@ -15,7 +18,7 @@ def index(page):
     products = Product.query(
         Product.is_public == True,
         Product.images_list.is_image == True).order(-Product.rating)
-    paginator = Paginator(products, 12)
+    paginator = Paginator(products, PRODUCT_PER_PAGE)
     try:
         products = paginator.page(page)
     except (EmptyPage, InvalidPage):
