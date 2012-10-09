6b1a9c417a8865e4455db19700dab2409513a36f
Serg Baburin <serg.baburin@gmail.com>
2012-10-10 01:00:39 +0400

    Used form.populate_obj()

--- admin_views.py
+++ admin_views.py
@@ -21,41 +21,15 @@ json_mod = flask.Blueprint(
 @mod.route('/', methods=['GET', 'POST'])
 @admin_required
 def config_update():
-    form = ConfigUpdateForm()
-
     config_db = model.Config.get_master_db()
+    form = ConfigUpdateForm(obj=config_db)
     if form.validate_on_submit():
-        config_db.analytics_id = form.analytics_id.data
-        config_db.brand_name = form.brand_name.data
-        config_db.facebook_app_id = form.facebook_app_id.data
-        config_db.facebook_app_secret = form.facebook_app_secret.data
-        config_db.feedback_email = form.feedback_email.data
-        config_db.flask_secret_key = form.flask_secret_key.data
-        config_db.pubnub_publish = form.pubnub_publish.data
-        config_db.pubnub_secret = form.pubnub_secret.data
-        config_db.pubnub_subscribe = form.pubnub_subscribe.data
-        config_db.twitter_consumer_key = form.twitter_consumer_key.data
-        config_db.twitter_consumer_secret = form.twitter_consumer_secret.data
         config_db.put()
         update_config_variables(config_db)
         flask.flash('Your Config settings have been saved', category='success')
         return flask.redirect(flask.url_for('admin.config_update'))
-    if not form.errors:
-        form.analytics_id.data = config_db.analytics_id
-        form.brand_name.data = config_db.brand_name
-        form.facebook_app_id.data = config_db.facebook_app_id
-        form.facebook_app_secret.data = config_db.facebook_app_secret
-        form.feedback_email.data = config_db.feedback_email
-        form.flask_secret_key.data = config_db.flask_secret_key
-        form.pubnub_publish.data = config_db.pubnub_publish
-        form.pubnub_secret.data = config_db.pubnub_secret
-        form.pubnub_subscribe.data = config_db.pubnub_subscribe
-        form.twitter_consumer_key.data = config_db.twitter_consumer_key
-        form.twitter_consumer_secret.data = config_db.twitter_consumer_secret
-
     if flask.request.path.startswith('/_json/'):
         return util.jsonify_model_db(config_db)
-
     return flask.render_template(
         'admin/config_update.html',
         title=u'Общие настройки',
