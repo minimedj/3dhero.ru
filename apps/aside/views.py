380ff14445d5ac3bf8e85545d67ce453a8cb50d0
Serg Baburin <serg.baburin@gmail.com>
2012-10-30 13:19:30 +0300

    Change category/series/brand classes.

--- apps/aside/views.py
+++ apps/aside/views.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
-from apps.product.models import Series
+from apps.product.models import Category
 
-def get_aside(active='series', key_id=None):
-    active = 'series'
+def get_aside(active='category', key_id=None):
     sections=[]
-    if active == 'series':
-        sections = Series.query(
-            Series.is_public == True,
-            Series.is_products == True
-        ).order(Series.name)
+    if active == 'category':
+        sections = Category.query(
+            Category.is_public == True
+        ).order(Category.name)
     if key_id:
         key_id = int(key_id)
     return render_template(
--- apps/product/models.py
+++ apps/product/models.py
@@ -14,14 +14,15 @@ def strip_string(s):
         if type_ == str:
             return ' '.join(s.split())
         if type_ == unicode:
-            return ' '.join(s.split())
+            return u' '.join(s.split())
     return s
 
-def rename_section(section, section_name):
-    if not section:
-        return
-    for product in section.all_products:
+def rename_section(section, section_product_cls, section_name):
+    section_products = section_product_cls.query(section_product_cls.section_key == section.key)
+    for section_product in section_products:
         product_mem_key = uuid()
+        if not section_product.product_key:
+            continue
         memcache.add(
             product_mem_key,
             {'%s' % section_name: section.name},
@@ -30,203 +31,178 @@ def rename_section(section, section_name):
         taskqueue.add(
             url=url_for(
                 'product.task.update_product',
-                key_id=product,
+                key_id=section_product.product_key.id(),
                 mem_key=product_mem_key
             ))
 
 class BaseSection(Base):
     name = ndb.StringProperty(verbose_name=u'Название', indexed=True, required=True)
     name_lowercase = ndb.StringProperty(indexed=True, default=None)
-
-    products = ndb.IntegerProperty(repeated=True)
-    hide_products = ndb.IntegerProperty(repeated=True)
-
-    is_products = ndb.ComputedProperty(lambda self: True if self.products_count else False)
-    is_hide_products = ndb.ComputedProperty(lambda self: True if self.hide_products_count else False)
-    is_all_products = ndb.ComputedProperty(lambda self: True if self.all_products_count else False)
-    products_count = ndb.ComputedProperty(lambda self: len(self.products))
-    hide_products_count = ndb.ComputedProperty(lambda self: len(self.hide_products))
-    all_products_count = ndb.ComputedProperty(lambda self: len(self.all_products))
-
     is_public = ndb.BooleanProperty(verbose_name=u'Показывать на сайте?', default=True)
 
     def _pre_put_hook(self):
         self.name = strip_string(self.name)
         lower_name = self.name.lower()
-        if lower_name != self.name_lowercase:
+        if self.key and lower_name != self.name_lowercase:
             self.do_rename()
         self.name_lowercase = lower_name
+
+
+    def pre_delete(self, section_product_cls):
+        sp = section_product_cls.query(section_product_cls.section_key == self.key)
+
 
     def do_rename(self):
         raise Exception("do_rename() not overloaded in the child class")
 
-    @property
-    def all_products(self):
-        return self.products + self.hide_products
-
-    @classmethod
-    def get_exist(cls, name):
-        if not name:
-            return None
-        is_exist = cls.query().filter(cls.name_lowercase == name.lower())
-        if is_exist.count():
-            return is_exist.get()
-        return None
-
     @classmethod
     def new_or_exist(cls, name):
-        is_exist = cls.get_exist(name)
+        is_exist = cls.retrieve_one_by('name_lowercase', strip_string(name).lower())
         if is_exist:
             return is_exist
         section = cls(name = name)
+        section.put()
         return section
 
-    @classmethod
-    def delete_product(cls, name, key_id):
-        is_exist = cls.get_exist(name)
-        if is_exist is None:
-            return False
-        flag = False
-        if key_id in is_exist.products:
-            is_exist.products.remove(key_id)
-            flag = True
-        if key_id in is_exist.hide_products:
-            is_exist.hide_products.remove(key_id)
-            flag = True
-        if flag:
-            is_exist.put()
+    def _public_product_count(self, section_product_cls):
+        return section_product_cls.query(
+            section_product_cls.section_key == self.key,
+            section_product_cls.is_public == True).count()
+
+    def _hide_product_count(self, section_product_cls):
+        return section_product_cls.query(
+            section_product_cls.section_key == self.key,
+            section_product_cls.is_public == False).count()
+
+    def _product_count(self, section_product_cls):
+        return section_product_cls.query(
+            section_product_cls.section_key == self.key).count()
+
+
+class SectionProduct(Base):
+    section_key = ndb.KeyProperty()
+    product_key = ndb.KeyProperty()
+    is_public = ndb.ComputedProperty(
+        lambda self:
+            self.product.is_public if self.product else False
+    )
+
+    @cached_property
+    def product(self):
+        if self.product_key:
+            return self.product_key.get()
+        return None
+
+    @cached_property
+    def section(self):
+        if self.section_key:
+            return self.section_key.get()
+        return None
+
+class CategoryProduct(SectionProduct):
+    pass
 
 class Category(BaseSection):
+    @cached_property
+    def public_product_count(self):
+        return self._public_product_count(CategoryProduct)
+
+    @cached_property
+    def hide_product_count(self):
+        return self._hide_product_count(CategoryProduct)
+
+    @cached_property
+    def product_count(self):
+        return self._product_count(CategoryProduct)
+
     def do_rename(self):
-        rename_section(self, 'category')
-
-    @classmethod
-    def _pre_delete_hook(cls, key):
-        category = key.get()
-        if category:
-            for product in category.all_products:
-                product_mem_key = uuid()
-                memcache.add(
-                    product_mem_key,
-                    {'category': ''},
-                    7200
-                )
-                taskqueue.add(
-                    url=url_for(
-                        'product.task.update_product',
-                        key_id=product,
-                        mem_key=product_mem_key)
-                )
+        if self.key != ndb.Key('Category', None):
+            rename_section(self, CategoryProduct, 'category')
 
 
-class Genre(BaseSection):
-    def do_rename(self):
-        rename_section(self, 'genre')
-
-    @classmethod
-    def _pre_delete_hook(cls, key):
-        genre = key.get()
-        if genre:
-            for product in genre.all_products:
-                product_mem_key = uuid()
-                memcache.add(
-                    product_mem_key,
-                    {'genre': ''},
-                    7200
-                )
-                taskqueue.add(
-                    url=url_for(
-                        'product.task.update_product',
-                        key_id=product,
-                        mem_key=product_mem_key)
-                )
-
-class Series(BaseSection):
-    def do_rename(self):
-        rename_section(self, 'series')
-
-    @classmethod
-    def _pre_delete_hook(cls, key):
-        series = key.get()
-        if series:
-            for product in series.all_products:
-                product_mem_key = uuid()
-                memcache.add(
-                    product_mem_key,
-                    {'series': ''},
-                    7200
-                )
-                taskqueue.add(
-                    url=url_for(
-                        'product.task.update_product',
-                        key_id=product,
-                        mem_key=product_mem_key)
-                )
+class BrandProduct(SectionProduct):
+    pass
 
 class Brand(BaseSection):
+    @cached_property
+    def public_product_count(self):
+        return self._public_product_count(BrandProduct)
+
+    @cached_property
+    def hide_product_count(self):
+        return self._hide_product_count(BrandProduct)
+
+    @cached_property
+    def product_count(self):
+        return self._product_count(BrandProduct)
+
     def do_rename(self):
-        rename_section(self, 'brand')
+        if self.key != ndb.Key('Brand', None):
+            rename_section(self, BrandProduct, 'brand')
 
-    @classmethod
-    def _pre_delete_hook(cls, key):
-        brand = key.get()
-        if brand:
-            for product in brand.all_products:
-                product_mem_key = uuid()
-                memcache.add(
-                    product_mem_key,
-                    {'brand': ''},
-                    7200
-                )
-                taskqueue.add(
-                    url=url_for(
-                        'product.task.update_product',
-                        key_id=product,
-                        mem_key=product_mem_key)
-                )
+class SeriesProduct(SectionProduct):
+    pass
 
-def _unique_section_products(section):
-    prs = section.products
-    hide_prs =section.hide_products
-    section.products = list(set(section.products))
-    section.hide_products = list(set(section.hide_products))
-    if prs != section.products or hide_prs != section.hide_products:
-        return True
-    return False
+class Series(BaseSection):
+    @cached_property
+    def public_product_count(self):
+        return self._public_product_count(SeriesProduct)
 
-def _clear_section(section, key_id):
-    flag = False
-    if section:
-        flag = _unique_section_products(section)
-        if key_id in section.products:
-            section.products.remove(key_id)
-            flag = True
-        if key_id in section.hide_products:
-            section.hide_products.remove(key_id)
-            flag = True
-    return flag
+    @cached_property
+    def hide_product_count(self):
+        return self._hide_product_count(SeriesProduct)
 
-def _set_section(section, key_id, is_public=True):
-    flag = False
-    if section:
-        if is_public:
-            if key_id in section.hide_products:
-                section.hide_products.remove(key_id)
-                flag = True
-            if key_id not in section.products:
-                section.products.append(key_id)
-                flag = True
-        else:
-            if key_id in section.products:
-                section.products.remove(key_id)
-                flag = True
-            if key_id not in section.hide_products:
-                section.hide_products.append(key_id)
-                flag = True
-    return flag
+    @cached_property
+    def product_count(self):
+        return self._product_count(SeriesProduct)
+
+    def do_rename(self):
+        if self.key != ndb.Key('Series', None):
+            rename_section(self, SeriesProduct, 'series')
+
+class CountryProduct(SectionProduct):
+    pass
+
+class Country(BaseSection):
+    @cached_property
+    def public_product_count(self):
+        return self._public_product_count(CountryProduct)
+
+    @cached_property
+    def hide_product_count(self):
+        return self._hide_product_count(CountryProduct)
+
+    @cached_property
+    def product_count(self):
+        return self._product_count(CountryProduct)
+
+    def do_rename(self):
+        if self.key != ndb.Key('Country', None):
+            rename_section(self, CountryProduct, 'country')
 
 class ProductImage(File):
     is_master = ndb.BooleanProperty(verbose_name=u'Основное изображение?', default=False)
+
+def set_section(section_cls, section_product_cls, product, section_name):
+    section_value = getattr(product, section_name)
+    section_product = section_product_cls.query(section_product_cls.product_key == product.key).get()
+    if section_product:
+        if not section_value:
+            section_product.key.delete()
+        section = section_cls.new_or_exist(section_value)
+        if section_product.section_key != section.key:
+            section_product.section_key = section.key
+            section_product.put()
+    else:
+        if section_value:
+            section = section_cls.new_or_exist(section_value)
+            section_product = section_product_cls(section_key=section.key, product_key=product.key)
+            section_product.put()
+
+def clear_section_product(section_product_cls, product_key):
+    section_products = section_product_cls.query(section_product_cls.product_key == product_key)
+    for section_product in section_products:
+        section_product.key.delete()
 
 class Product(Base):
     id_1c = ndb.StringProperty(verbose_name=u'Код 1С', indexed=True)
@@ -234,11 +210,10 @@ class Product(Base):
     barcode = ndb.StringProperty(verbose_name=u'Штрих код', default='', indexed=True)
 
     name = ndb.StringProperty(verbose_name=u'Название', default='', indexed=True)
-    category = ndb.StringProperty(verbose_name=u'Категория', default='', indexed=True)
-    genre = ndb.StringProperty(verbose_name=u'Жанр', default='', indexed=True)
-    series = ndb.StringProperty(verbose_name=u'Серия', default='', indexed=True)
 
-    brand = ndb.StringProperty(verbose_name=u'Брэнд/Производитель', indexed=True)
+    category = ndb.StringProperty(verbose_name=u'Категория', default='', indexed=True)
+    brand = ndb.StringProperty(verbose_name=u'Бренд/Производитель', indexed=True)
+
     country = ndb.StringProperty(verbose_name=u'Страна', default='', indexed=True)
 
     rating = ndb.IntegerProperty(verbose_name=u'Рейтинг')
@@ -266,6 +241,7 @@ class Product(Base):
 
     badge = ndb.StringProperty(verbose_name=u'Бэйдж', indexed=False)
     description = ndb.TextProperty(verbose_name=u'Описание', default='')
+    equipment = ndb.TextProperty(verbose_name=u'Комплектация', default='')
 
     images_list = ndb.StructuredProperty(ProductImage, repeated=True)
 
@@ -275,8 +251,6 @@ class Product(Base):
         'barcode',
         'name',
         'category',
-        'genre',
-        'series',
         'brand',
         'country',
         'rating',
@@ -295,6 +269,7 @@ class Product(Base):
         'leftovers_on_way',
         'receipt_date',
         'description',
+        'equipment',
         'images'
     ])
 
@@ -306,63 +281,9 @@ class Product(Base):
     def images(self):
         return [img.url for img in self.images_list]
 
-    @ndb.toplevel
-    def clear_sections(self):
-        try:
-            key_id = self.key.id()
-        except:
-            return False
-        flag = False
-        category = Category.get_exist(self.category)
-        if _clear_section(category, key_id):
-            category.put_async()
-            flag = True
-        genre = Genre.get_exist(self.genre)
-        if _clear_section(genre, key_id):
-            genre.put_async()
-            flag = True
-        series = Series.get_exist(self.series)
-        if _clear_section(series, key_id):
-            series.put_async()
-            flag = True
-        brand = Brand.get_exist(self.brand)
-        if _clear_section(brand, key_id):
-            brand.put_async()
-            flag = True
-        return flag
-
-    def set_sections(self):
-        try:
-            key_id = self.key.id()
-        except:
-            return False
-        flag = False
-        if self.category:
-            category = Category.new_or_exist(self.category)
-            if _set_section(category, key_id, self.is_public):
-                category.put()
-                flag = True
-        if self.genre:
-            genre = Genre.new_or_exist(self.genre)
-            if _set_section(genre, key_id, self.is_public):
-                genre.put()
-                flag = True
-        if self.series:
-            series = Series.new_or_exist(self.series)
-            if _set_section(series, key_id, self.is_public):
-                series.put()
-                flag = True
-        if self.brand:
-            brand = Brand.new_or_exist(self.brand)
-            if _set_section(brand, key_id, self.is_public):
-                brand.put()
-                flag = True
-        return flag
 
     def _pre_put_hook(self):
         self.category = strip_string(self.category)
-        self.genre = strip_string(self.genre)
-        self.series = strip_string(self.series)
         self.badge = strip_string(self.badge)
         self.barcode = strip_string(self.barcode)
         self.brand = strip_string(self.brand)
@@ -370,13 +291,17 @@ class Product(Base):
         self.material = strip_string(self.material)
 
     def _post_put_hook(self, future):
-        self.set_sections()
+        set_section(Category, CategoryProduct, self, 'category')
+        set_section(Brand, BrandProduct, self, 'brand')
+        set_section(Country, CountryProduct, self, 'country')
 
     @classmethod
     def _pre_delete_hook(cls, key):
+        clear_section_product(CategoryProduct, key)
+        clear_section_product(BrandProduct, key)
+        clear_section_product(CountryProduct, key)
         p = key.get()
         if p:
-            p.clear_sections()
             for img in p.images_list:
                 img.delete_blob()
 
