28b4006f9eb820db57e0af2906e12df9f2ebb655
Serg Baburin <serg.baburin@gmail.com>
2014-11-30 23:36:45 +0300

    Reorder imports. Reformat code.

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, redirect, render_template, request, url_for, session
-from apps.search.forms import SearchForm
-from auth import is_admin
-from apps.product.models import Product, Category
-from util import get_next_url, param
 from google.appengine.ext import ndb
 from google.appengine.api import memcache
+from flask import Blueprint, redirect, render_template, url_for, session
+
+from apps.search.forms import SearchForm
 from apps.pages.views import get_paginator, PRODUCT_VIEW_TYPES
+from apps.product.models import Product, Category
+from auth import is_admin
+import util
 
 
 mod = Blueprint(
@@ -23,14 +24,16 @@ def index_form():
     if form.validate_on_submit():
         query = form.query.data.lower()
         return redirect(url_for('search.index', query=query))
-    return redirect(get_next_url())
+    return redirect(util.get_next_url())
+
 
 @mod.route('/<string:query>/', defaults={'page': 1})
 @mod.route('/<string:query>/page/<int:page>/')
 def index(query, page):
-    view_type = param('product_view_type', int)
+    view_type = util.param('product_view_type', int)
     session_view_type = session.get('product_view_type', None)
-    if session_view_type is None or session_view_type not in PRODUCT_VIEW_TYPES.itervalues():
+    if session_view_type is None or\
+            session_view_type not in PRODUCT_VIEW_TYPES.itervalues():
         if view_type in PRODUCT_VIEW_TYPES.itervalues():
             session['product_view_type'] = view_type
         else:
