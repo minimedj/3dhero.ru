4499ec4d018aeea96e4a675779e1c1f1f9903639
Serg Baburin <serg.baburin@gmail.com>
2014-12-01 00:13:01 +0300

    Memcached of the search result and refactoring.

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from google.appengine.ext import ndb
-from google.appengine.api import memcache
 from flask import Blueprint, redirect, render_template, url_for, session
 
 from apps.search.forms import SearchForm
 from apps.pages.views import get_paginator, PRODUCT_VIEW_TYPES
 from apps.product.models import Product, Category
-from auth import is_admin
 import util
 
 
@@ -27,6 +25,43 @@ def index_form():
     return redirect(util.get_next_url())
 
 
+@util.memcached()
+def search_categories(query):
+    categories_q = Category.query().fetch(projection=[Category.name])
+    categories_keys = [
+        c.key for c in categories_q if query in c.name.lower()
+    ]
+    if categories_keys:
+        return ndb.get_multi(categories_keys)
+    return []
+
+
+@util.memcached()
+def get_searchable_products():
+    products_q = Product.query()
+    return products_q.fetch(
+        projection=[Product.name, Product.barcode, Product.catalogue_id]
+    )
+
+
+@util.memcached()
+def search_products(query):
+    products_q = get_searchable_products()
+    ids = []
+    for product in products_q:
+        if query in product.name.lower() \
+                or query in product.barcode \
+                or query in product.catalogue_id:
+            ids.append(product.key)
+    if ids:
+        return ndb.get_multi(ids)
+    return []
+
+
+def search_products_paginator(products, page):
+    return get_paginator(products, page, product_per_page=20)
+
+
 @mod.route('/<string:query>/', defaults={'page': 1})
 @mod.route('/<string:query>/page/<int:page>/')
 def index(query, page):
@@ -42,50 +77,22 @@ def index(query, page):
         if view_type in PRODUCT_VIEW_TYPES.itervalues():
             session['product_view_type'] = view_type
 
-    products = []
-    categories = []
     query = query.strip().lower()
     if not query:
         return render_template(
             'search/index.html',
             title=u"Поиск по сайту",
             query=query,
-            categories=categories,
-            products=products
+            categories=[],
+            products=[]
         )
-    categories_q = Category.query().fetch(projection=[Category.name])
-    cids = memcache.get('search_cids-%s' % query)
-    if not cids:
-        cids = []
-        for category in categories_q:
-            if query in category.name.lower():
-              cids.append(category.key)
-        memcache.add('search_cids-%s' % query, cids, 600)
-    if cids:
-        categories = ndb.get_multi(cids)
-    if is_admin():
-        products_q = Product.query()
-    else:
-        products_q = Product.query(Product.is_available == True)
-    products_q = products_q.fetch(
-        projection=[Product.name, Product.barcode, Product.catalogue_id]
-    )
-    ids = memcache.get('search_ids-%s' % query)
-    if not ids:
-        ids = []
-        for product in products_q:
-            if query in product.name.lower()\
-                    or query in product.barcode\
-                    or query in product.catalogue_id:
-                ids.append(product.key)
-        memcache.add('search_ids-%s' % query, ids, 600)
-    if ids:
-        products = ndb.get_multi(ids)
-        products = get_paginator(products, page, product_per_page=20)
+    products = search_products(query)
+    if products:
+        products = search_products_paginator(products, page)
     return render_template(
         'search/index.html',
         title=u'Поиск по сайту',
         query=query,
-        categories=categories,
+        categories=search_categories(query),
         products=products
     )
